c3f2f658696c67d013b6f49d73e535a74339b894
PhilippMatthes <github@philippmatth.es>
2020-12-21 14:32:51 +0100

    Fix password not being set in registration

--- fragenautomat/profiles/forms.py
+++ fragenautomat/profiles/forms.py
@@ -21,6 +21,7 @@ class RegistrationForm(UserCreationForm):
     def save(self, commit=True):
         user = super(UserCreationForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.set_password(self.cleaned_data["password1"])
         if commit:
             user.save()
         return user
